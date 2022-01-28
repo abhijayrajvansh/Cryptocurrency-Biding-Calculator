2763296029d9b78c3cbef02bef3b6b2eec9f0d12
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2022-01-29 02:26:19 +0530

    Updating Files

---
+++ app-intro.sh
@@ -0,0 +1,24 @@
+Orange='\033[0;31m'
+NC='\033[0m' # No Color
+echo " "
+echo -e "${Orange}@@@@@@@@@@@*******************@@@@@@@@@@${NC}"
+echo -e "${Orange}@@@@@@@***************************@@@@@@${NC}"
+echo -e "${Orange}@@@@@**************,  **************@@@@${NC}              Cryptocurrency Biding Calculator V 1.1"
+echo -e "${Orange}@@@***********   **  **  *************@@${NC}              ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo -e "${Orange}@@**************          *************@${NC}"
+echo -e "${Orange}@***************    ****     ***********${NC}               OS         : Mac OSx"
+echo -e "${Orange}@**************    ******    ***********${NC}               Platform   : Running on Terminal"
+echo -e "${Orange}***************            *************${NC}               Exchange   : NOT Logged In"
+echo -e "${Orange}@*************    ******     ***********${NC} "
+echo -e "${Orange}@**********       ******     ***********${NC}"
+echo -e "${Orange}@@*************            ************@${NC}"
+echo -e "${Orange}@@@***********  **  ******************@@${NC}              Copyright © All Rights Reserved by"
+echo -e "${Orange}@@@@@*******************************@@@@${NC}                       Abhijay Rajvansh"
+echo -e "${Orange}@@@@@@@***************************@@@@@@${NC}"
+echo -e "${Orange}@@@@@@@@@@@*******************@@@@@@@@@@${NC}        Contact Developer : abhijayrajvansh01@gmail.com"
+echo " "
+echo "-----------------------------------------------------------------------------------------------------"
+sleep 1
+echo "Running Files..."
+alias python='python3'
+python app.py
